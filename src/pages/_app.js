f218c822b77c82918fad154921faddde345bf5d4
3sbien <3sbien@gmail.com>
2025-10-09 23:19:49 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,9 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <header style={{padding:16,borderBottom:"1px solid #eee"}}>
+        <a href="/"><img src="/Qabum%20logo.png" alt="Qabum" style={{height:32}} /></a>
+      </header>
       <Component {...pageProps} />
     </SessionProvider>
   );
