4f5dacda73f7c4351304deffbb9fc030cd8bbfc7
3sbien <3sbien@gmail.com>
2025-10-10 01:14:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
+import { signIn } from "next-auth/react";
 
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Qabum™ Marketplace</h1>
+      <p>Este es el MVP oficial de demostración / This is the official MVP demo.</p>
+      <button onClick={() => signIn("google")}>Ingresar / Sign in with Google</button>
+    </main>
+  );
+}
