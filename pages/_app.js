ba74ed3ae23770932ef94042f7e4a2030bc2fd0f
3sbien <3sbien@gmail.com>
2025-10-12 20:35:30 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
+import "../src/styles/globals.css";
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <header style={{padding:16,borderBottom:"1px solid #eee"}}>
-        <a href="/"><img src="/Qabum%20logo.png" alt="Qabum" style={{height:32}} /></a>
+      <header style={{ padding: 16, borderBottom: "1px solid #eee" }}>
+        <a href="/"><img src="/Qabum%20logo.png" alt="Qabum" style={{ height: 32 }} /></a>
       </header>
       <Component {...pageProps} />
     </SessionProvider>
